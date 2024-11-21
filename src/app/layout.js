2a155a686954d61d12f0729e2e0c1aab3ac52a63
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-11-21 10:22:08 +0530

    Change favicon icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        {/* Favicon */}
+        <link rel="icon" href="/sync-draw-logo.png" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
